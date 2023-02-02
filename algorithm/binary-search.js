92ec017a387b0651e79eaed4b63281336f234f20
Eureka Labdawara <38484773+EurekaLabdawara@users.noreply.github.com>
2023-02-02 15:42:37 +0700

    add solution 2

--- algorithm/binary-search.js
+++ algorithm/binary-search.js
@@ -1,16 +1,16 @@
 let input = [...Array(10).keys()]
 
 function binarySearch(arr, target, start= 0, end = arr.length -1) {
+    // console.log("str: ",start);
+    // console.log("emd: ",end);
+    // console.log("target: ",target);
     if (start > end) {
         console.log('Not found!');
         return -1;
     } 
     let middle = Math.floor((start+end)/2) 
-    // console.log("str: ",start);
     // console.log("mid: ",middle);
-    // console.log("emd: ",end);
     // console.log("val: ",arr[middle]);
-    // console.log("target: ",target);
     if (arr[middle] < target) {
         // console.log("right");
         return binarySearch(arr, target,middle+1,end)
